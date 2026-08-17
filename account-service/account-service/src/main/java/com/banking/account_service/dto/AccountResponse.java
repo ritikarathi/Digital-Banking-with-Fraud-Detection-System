5c7c479358dd5650d850b19d339edcfed133b38b
Ritika <ritikarathi2005@gmail.com>
2026-08-18 00:13:09 +0530

    handling accountservice

--- account-service/account-service/src/main/java/com/banking/account_service/dto/AccountResponse.java
+++ account-service/account-service/src/main/java/com/banking/account_service/dto/AccountResponse.java
@@ -1,4 +1,36 @@
 package com.banking.account_service.dto;
 
+import com.banking.account_service.entity.AccountStatus;
+import com.banking.account_service.entity.AccountType;
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 public class AccountResponse {
+
+    private String id;
+
+
+    private String accountNumber;
+
+    private String accountHolderName;
+
+    private String email;
+
+    private String phone;
+
+    @Column(nullable = false)
+    private AccountType accountType;
+
+    @Column(nullable = false)
+    private AccountStatus accountStatus;
+
+    private BigDecimal balance;
+
+    private BigDecimal DailyTransactionLimit;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
 }
--- account-service/account-service/src/main/java/com/banking/account_service/dto/CreateAccountRequest.java
+++ account-service/account-service/src/main/java/com/banking/account_service/dto/CreateAccountRequest.java
@@ -1,12 +1,37 @@
 package com.banking.account_service.dto;
 
+import com.banking.account_service.entity.AccountType;
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class CreateAccountRequest {
+
+    @NotBlank(message = "Account Holder Name is required")
+    private String accountHolderName;
+
+    @NotBlank(message = "Email is Required")
+    private String email;
+
+    @NotBlank(message = "phone is required")
+    private String phone;
+
+    @NotNull(message = "AccountType is Required")
+    private AccountType accountType;
+
+    @NotNull(message = "initial deposit is required")
+    @Positive(message = "initial deposit must be positive")
+    private BigDecimal initialDeposit;
 
 }
